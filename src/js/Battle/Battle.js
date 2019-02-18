800981a112f83e4e75178465d5d3a0eb1e007828
Houlgatte Louka <lhoulgattebustamante@gmail.com>
2019-02-18 15:58:44 +0100

    bug fix in battle.js

--- src/js/Battle/Battle.js
+++ src/js/Battle/Battle.js
@@ -1,30 +1,31 @@
-import Player from '../Player/Player';
-import Actor from '../Actor/Actor.js';
-import Item from '../Item/Item.js';
-
 class Battle {
-  constructor(players, monster){
-    this.players=players;
-    this.monster=monster;
+  constructor(players, monster) {
+    this.players = players;
+    this.monster = monster;
   }
 
-  comparePosition(){
+  comparePosition() {
     return true;
   }
 
-  startFight(players, monster){
-      if(this.comparePosition() == true){
-        while(monster.hp > 0){
-          players.forEach(player => {
-            player.fight(monster);
-          });
-          playerSelected = getRandomInt(players.length + 1)
-          if(players[playerSelected].hp > 0){
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  startFight(players, monster) {
+    if (this.comparePosition() === true) {
+      while (monster.hp > 0) {
+        players.forEach(player => {
+          player.fight(monster);
+        });
+        const playerSelected = this.getRandomInt(players.length + 1);
+        if (players[playerSelected].hp > 0) {
           monster.fight(players[playerSelected]);
         }
-        }
-      }    
+      }
+    }
   }
 }
+
 
 export default Battle;
