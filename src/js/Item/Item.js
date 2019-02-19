78e6d07729619a890abd526f6ec4a6efa7145fdd
Remi- <remi-castel@hotmail.fr>
2019-02-19 13:54:52 +0100

    supp du code nul

--- src/js/Item/Item.js
+++ src/js/Item/Item.js
@@ -7,15 +7,6 @@ this.Hp = Hp;
 this.Damage = Damage;
 }
 
-creeItem(){
-let Nom = prompt("Entrez le nom de l'item");
-let Description = prompt("Entrez une description");
-let Hp = prompt("Entrez l'ajout de points de vie qu'apporte l'item");
-let Damage = prompt("Entrez l'ajout de dégats qu'apporte l'item");
-let item = new Item(Nom,Description,Hp,Damage);
-prompt(item);
-}
-
 }
 
 export default Item;
